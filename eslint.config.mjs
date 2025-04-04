9a88622c4dac1c6aacadff870a0ef87bd30cde02
AleksTikhonov <122445936+rufijey@users.noreply.github.com>
2025-04-04 18:50:28 +0300

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,7 +31,6 @@ export default tseslint.config(
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-floating-promises': 'warn',
         '@typescript-eslint/no-unsafe-argument': 'warn',
-        'prettier/prettier': ['error', { endOfLine: 'lf' }],
       },
     },
 );
